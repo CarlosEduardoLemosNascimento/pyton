5d456c4b436143f8672b83788a41501c97e490fc
CarlosEduardoLemosNascimento <159453764+CarlosEduardoLemosNascimento@users.noreply.github.com>
2024-05-07 23:23:22 +0000

    Exercícios de laços de repetição

--- 3. Laços  de Repetição/Exercicio 15.py
+++ 3. Laços  de Repetição/Exercicio 15.py
@@ -10,21 +10,26 @@ os.system("cls || clear")
 numero: int
 contaPar: int
 contaImpar: int
+contaNulo: int
 i: int
 
 # Pedindo os números ao usuário
 os.system("cls || clear")
 contaPar=0
 contaImpar=0
+contaNulo=0
 
 for i in range(1, 6):
     numero=int(input(f"Digite o {i}º número - inteiro diferente de zero, de 5: "))
     # Testando se o número é par ou impar e incrementando o contador correspondente
+    if numero==0:
+        contaNulo=contaNulo+1
     if numero%2==0:
         contaPar=contaPar+1
     else:
         contaImpar=contaImpar+1
 # Exibindo resultados
 print ("\n")
+print(f"Quantidade de zeros digitados: {contaNulo}")
 print(f"Quantidade de números pares: {contaPar}")
 print(f"Quantidade de números ímpares: {contaImpar}")
